fd9be60dc973f2e7417236783902d873ca0f877a
RamiroRibeiro <113456039+RamiroRibeiro@users.noreply.github.com>
2024-02-01 09:30:44 -0400

    Add files via upload

--- envia.php
+++ envia.php
@@ -1,22 +1,20 @@
 <?php
 
-    $nome = addcslashes($_POST['nome']);
-    $email = addcslashes($_POST['email']);
-    $contato = addcslashes($_POST['contato']);
+$nome = addslashes($_POST['nome']);
+$email = addslashes($_POST['email']);
+$contato = addslashes($_POST['contato']);
 
-    $destino = "sistemas@dmarcos.com.br";
-    $assunto = "coleta de dados";
+$destino = "sistemas@dmarcos.com.br";
+$assunto = "coleta de dados";
 
+$corpo = "Nome: " . $nome . "\n" . "Email: " . $email . "\n" . "Contato: " . $contato;
 
-    $corpo = "Nome: ".$nome."\n"."Email: ".$email."\n"."Contato: ".$contato;
+$cabeca = "From: ribeiro.ramiro6@gmail.com" . "\n" . "Reply-to: " . $email . "\n" . "X-Mailer:PHP/" . phpversion();
 
-    $cabeca = "From: ribeiro.ramiro6@gmail.com"."\n"."Reply-to: ".$email."\n"."x=mailer:PHP/".phpversion();
-
-    if(mail($para,$assunto,$corpo,$cabeca)){
-        echo("E-mail enviado com sucesso");
-    }else{
-        echo("Houve um erro ao enviar o email");
-    }
-    
+if (mail($destino, $assunto, $corpo, $cabeca)) {
+    echo ("E-mail enviado com sucesso");
+} else {
+    echo ("Houve um erro ao enviar o email");
+}
 
 ?>
